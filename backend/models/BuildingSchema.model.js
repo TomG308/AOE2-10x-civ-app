9b4578b674ef00776ab8263f4d685a75f0f9e33f
Thomas Tate <tate.thomas5595@gmail.com>
2024-10-29 22:11:32 -0600

    Update BuildingSchema.model.js

---
+++ backend/models/BuildingSchema.model.js
@@ -0,0 +1,38 @@
+const BuildingSchema = new mongoose.Schema({
+    accuracyPercent: { type: Number },
+    armours: [
+      {
+        amount: { type: Number },
+        class: { type: Number },
+      },
+    ],
+    attack: { type: Number },
+    attacks: [
+      {
+        amount: { type: Number },
+        class: { type: Number },
+      },
+    ],
+    cost: {
+      wood: { type: Number },
+      stone: { type: Number },
+      gold: { type: Number },
+      food: { type: Number },
+    },
+    garrisonCapacity: { type: Number },
+    hp: { type: Number },
+    id: { type: Number, unique: true },
+    languageHelpId: { type: Number },
+    languageNameId: { type: Number },
+    lineOfSight: { type: Number },
+    meleeArmor: { type: Number },
+    minRange: { type: Number },
+    pierceArmor: { type: Number },
+    range: { type: Number },
+    reloadTime: { type: Number },
+    trainTime: { type: Number },
+    internal_name: { type: String },
+  });
+  
+  module.exports = mongoose.model('Building', BuildingSchema);
+  
